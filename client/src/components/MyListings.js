a98c4ca90a40e430bd27f2a7b653c0ee5a0856a9
abraham <apolichenco@gmail.com>
2023-02-07 17:33:20 -0500

    my listings complete, needs alot of css

--- client/src/components/MyListings.js
+++ client/src/components/MyListings.js
@@ -8,7 +8,18 @@ function MyListings({user, data}) {
 
     return (
         <div>
-
+            {thisUserListings.map((listing, index) => {
+                return (
+                    <div key={index}>
+                    <h4>Description:</h4>
+                    <p>
+                        Color: {listing.earing.color} <br></br>
+                        Shape: {listing.earing.shape}
+                    </p>
+                    <h3>Price: ${listing.price}</h3>
+                </div>
+                )
+            })}
         </div>
     )
 }
