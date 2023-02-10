f70eae4ccdb63a3958bd0bdffaa6bc831f6d8754
abraham <apolichenco@gmail.com>
2023-02-09 23:06:13 -0500

    added uniqueness with scope to earings, fix the capitalize

--- app/models/earing.rb
+++ app/models/earing.rb
@@ -1,7 +1,7 @@
 class Earing < ApplicationRecord
 
     has_many :listings
-    validates :color, presence: true 
+    validates :color, presence: true, uniqueness: {scope: :shape}
     validates :shape, presence: true
 
     def capitalized_shape
